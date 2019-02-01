3871276154efa8393d7c8a5c385a938843b50979
Saran Chockan <saranjitharunesh@gmail.com>
2019-01-31 23:15:12 -0600

    added soundcloud embedded player

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+
+$(function(){
+    responsiveScreen()
+})
+
 $(window).on('resize', function(){
+    responsiveScreen()
+});
+
+function responsiveScreen(){
     if($(window).width() <1400){
         $("#animated-thumbnials").css({
             "all" : "unset"
         })
     }
-
     else{
         $("#animated-thumbnials").css({
             "all" : ""
         })
     }
-});
+
+    var height = $("#animated-thumbnials").height() + 250 + "px"
+    $("#YamPics").css({
+        "height" : height
+    })
+}
