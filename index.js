780b4ff3f937071c2b3dabf0d87a0cb8b9e57257
Saran Chockan <saranjitharunesh@gmail.com>
2019-02-17 10:45:19 -0600

    added responsiveness to animated text

--- index.js
+++ index.js
@@ -2,9 +2,49 @@
 /* When website loads */
 
 $(function () {
+    if(window.innerWidth <635){
+        $(".ml4").css({
+            "position": "relative", "font-weight": "900","font-size": "1em"
+        })
+    }
 
+    else if(window.innerWidth <1052){
+        $(".ml4").css({
+            "position": "relative", "font-weight": "900","font-size": "2em"
+        })
+    }
+
+    else{
+        $(".ml4").css({
+            "position": "relative", "font-weight": "900","font-size": "3em"
+        })
+    }
 })
 
+/* When website resizes */
+
+
+$(window).resize(function () {
+    if(window.innerWidth <635){
+        $(".ml4").css({
+            "position": "relative", "font-weight": "900","font-size": "1em"
+        })
+    }
+
+    else if(window.innerWidth <1052){
+        $(".ml4").css({
+            "position": "relative", "font-weight": "900","font-size": "2em"
+        })
+    }
+
+    else{
+        $(".ml4").css({
+            "position": "relative", "font-weight": "900","font-size": "3em"
+        })
+    }
+})
+
+/* Animation jQuery plugin */
 
 $("a").click(function () {
     console.log("Scroll")
